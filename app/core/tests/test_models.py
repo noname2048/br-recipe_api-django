14fa68ce8ab9c1592c3fcdeb8a2977078074c339
noname2048 <sungwook.csw@gmail.com>
2022-07-18 23:26:03 +0900

    feat: raise when create user with blank email

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -32,3 +32,8 @@ class ModelTest(TestCase):
         for email, expected in sample_email:
             user = get_user_model().objects.create_user(email, "sample123")
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raise_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "test123")
